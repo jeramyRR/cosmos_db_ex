0c0bf81da16956ed1719a310778e0ed9ba42c83f
Jeramy Singleton <jeramy.singleton@microsoft.com>
2021-05-31 19:15:24 +0000

    Update version

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CosmosDbEx.MixProject do
   def project do
     [
       app: :cosmos_db_ex,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.11",
       description: description(),
       package: package(),
